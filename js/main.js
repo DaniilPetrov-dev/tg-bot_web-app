bbdcbf22e3949cfe9a4b2754c6bbedf122a60f3f
DaniilPetrov-dev <ichitrigerfingern1ggas@gmail.com>
2022-05-22 20:39:37 +0300

    The third commit. 1.1v. Added the output of the basket on top of the showcase. Added quantity control buttons to the cart.

--- js/main.js
+++ js/main.js
@@ -19,6 +19,85 @@ function goodsOut(data) {
 	$('.add-to-cart').on('click', addToCart);
 }
 
+
+// Код интерактивной корзины
+
+function loadCart() {
+	if (localStorage.getItem('cart')) {
+		cart = JSON.parse(localStorage.getItem('cart'));
+			showCart();
+	} 
+	else {
+		$('.mini-cart').html('Корзина пуста');
+	}
+}
+
+function showCart() {
+	if (!isEmply(cart)) {
+		$('.mini-cart').html('Корзина пуста');
+	}
+	else {
+	$.getJSON('goods.json', function (data) {
+		var goods = data;
+		var out = '';
+		for (var id in cart) {
+			out += `<button data-id="${id}" class="del-goods">Х</button>`;
+			out += `<img src="./image/${goods[id].img}" alt="">`;
+			out += ` ${goods[id].name}`;
+			out += `<button data-id="${id}" class="reduce-goods">-</button>`;
+			out += ` ${cart[id]}`
+			out += `<button data-id="${id}" class="add-goods">+</button>`;
+			out += cart[id]*goods[id].cost;
+			out += `<br>`;
+		}
+		$('.mini-cart').html(out);
+		$('.del-goods').on('click', delGoods);
+		$('.add-goods').on('click', addGoods);
+		$('.reduce-goods').on('click', reduceGoods);
+	});
+	}
+}
+
+function isEmply(object) {
+	for (var key in object) 
+	if (object.hasOwnProperty(key)) return true;
+	return false;
+}
+
+function addGoods() {
+	var id = $(this).attr('data-id');
+	cart[id]++
+	saveCart();
+	showCart();
+}
+
+function reduceGoods() {
+	var id = $(this).attr('data-id');
+	if(cart[id] == 1) {
+		delete cart[id];
+	}
+	 else {
+		cart[id]--;
+	}
+	saveCart();
+	showCart();
+}
+
+function delGoods() {
+	var id = $(this).attr('data-id');
+	delete cart[id];
+	saveCart();
+	showCart();
+}
+
+function saveCart() {
+	localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+
+//
+
+
 function addToCart() {
 	var id = $(this).attr('data-id');
 	// Если в корзине еще нет такого товара, то кладем один. Есил есть, увеличиваем на один.
@@ -29,8 +108,8 @@ function addToCart() {
 	else {
 		cart[id]++;
 	}
-	console.log(cart);
-	showMiniCart();
+	//console.log(cart);
+	showCart();
 	saveCart();
 }
 
@@ -38,13 +117,7 @@ function saveCart() {
 	localStorage.setItem('cart', JSON.stringify(cart));
 }
 
-function showMiniCart () {
-	var out ='';
-	for (var key in cart) {
-		out += key + ' - ' + cart[key]+'<br>';
-	}
-	$('.mini-cart').html(out);
-}
+
 
 function loadCart() {
 	if (localStorage.getItem('cart')) {
